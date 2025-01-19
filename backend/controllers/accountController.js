9d8de8a3592bfb903f68e8ba5c88a87f78b85328
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-01-19 02:56:23 +0100

    Account controller

---
+++ backend/controllers/accountController.js
@@ -0,0 +1,89 @@
+const asyncHandler = require("express-async-handler");
+const Account = require("../models/AccountModel");
+
+// @desc    Get all Accounts
+// @route   GET /api/accounts
+// @access  Public
+const getAccounts = asyncHandler(async (req, res) => {
+  const accounts = await Account.find();
+
+  res.status(200).json(accounts);
+});
+
+// @desc    Get single Account
+// @route   GET /api/accounts/:id
+// @access  Public
+const getAccount = asyncHandler(async (req, res) => {
+  const account = await Account.findById(req.params.id);
+
+  if (!account) {
+    res.status(404);
+    throw new Error("Account not found");
+  }
+
+  res.status(200).json(account);
+});
+
+// @desc    Create an Account
+// @route   POST /api/accounts
+// @access  Public
+
+const addAccount = asyncHandler(async (req, res) => {
+  if (!req.body.name || !req.body.type) {
+    res.status(400);
+    throw new Error("Please fill all fields!");
+  }
+  const account = await Account.create({
+    user: req.user._id,
+    name: req.body.name,
+    type: req.body.type,
+    balance: req.body.balance,
+    currency: req.body.currency,
+  });
+
+  res.status(201).json(account);
+});
+
+// @desc    Update an Account
+// @route   PUT /api/accounts/:id
+// @access  Public
+const updateAccount = asyncHandler(async (req, res) => {
+  const account = await Account.findById(req.params.id);
+
+  if (!account) {
+    res.status(404);
+    throw new Error("Account not found");
+  }
+
+  account.name = req.body.name || account.name;
+  account.type = req.body.type || account.type;
+  account.balance = req.body.balance || account.balance;
+  account.currency = req.body.currency || account.currency;
+  account.updatedAt = Date.now();
+
+  await account.save();
+
+  res.status(200).json(account);
+});
+
+// @desc    Delete an Account
+const deleteAccount = asyncHandler(async (req, res) => {
+  const account = await Account.findById(req.params.id);
+
+  if (!account) {
+    res.status(404);
+    throw new Error("Account not found");
+  }
+
+  await account.remove();
+
+  res.status(200).json({ message: "Account removed" });
+});
+
+module.exports = {
+  getAccounts,
+  getAccount,
+  addAccount,
+  updateAccount,
+  deleteAccount,
+};
