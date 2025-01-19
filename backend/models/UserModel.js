14f8276cf91de4647a5eac6609442d95c7619164
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-01-19 01:38:37 +0100

    UserSchema

---
+++ backend/models/UserModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "email is required"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+    minlength: 6,
+  },
+  currency: {
+    type: String,
+    default: "USD",
+    enum: ["USD", "EUR", "GBP", "NGN", "MAD"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
