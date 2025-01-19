99990c5bbe03ad481e871c8cdc9f111536a15337
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-01-19 01:48:50 +0100

    Account Schema

---
+++ backend/models/AccountModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const AccountSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+    },
+    type: {
+      type: String,
+      required: [true, "Type is required"],
+      enum: ["Cash", "Bank", "Asset", "Credit Card", "Loan"],
+    },
+    balance: {
+      type: Number,
+      default: 0,
+    },
+    currency: {
+      type: String,
+      default: "USD",
+      enum: ["USD", "EUR", "GBP", "NGN", "MAD"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Account", AccountSchema);
