89c0dab7b5e6e293ec08c471d2e2debc818d02f0
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-01-19 01:46:08 +0100

    User Schema Update

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,34 +1,39 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is required"],
-    trim: true,
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+      minlength: 6,
+    },
+    currency: {
+      type: String,
+      default: "USD",
+      enum: ["USD", "EUR", "GBP", "NGN", "MAD"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  email: {
-    type: String,
-    required: [true, "email is required"],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "password is required"],
-    minlength: 6,
-  },
-  currency: {
-    type: String,
-    default: "USD",
-    enum: ["USD", "EUR", "GBP", "NGN", "MAD"],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    timestamps: true, // Automatically add createdAt and updatedAt fields
+  }
+);
 
 module.exports = mongoose.model("User", UserSchema);
