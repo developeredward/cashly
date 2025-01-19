178b891b2e4a6eb085cd939cccce74d6ae3f6a9a
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-01-19 17:02:44 +0100

    User Routes Update

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,27 +1,20 @@
 const router = require("express").Router();
 
 const {
-  authUser,
-  registerUser,
-  getUserProfile,
-  updateUserProfile,
+  addUser,
+  getUser,
+  updateUser,
   getUsers,
   deleteUser,
-  getUserById,
-  updateUser,
 } = require("../controllers/userController");
 const { protect, admin } = require("../middleware/authMiddleware");
 
-router.route("/").post(registerUser).get(protect, admin, getUsers);
-router.post("/login", authUser);
-router
-  .route("/profile")
-  .get(protect, getUserProfile)
-  .put(protect, updateUserProfile);
+router.route("/").post(addUser).get(protect, admin, getUsers);
+router.route("/profile").get(protect, getUser).put(protect, updateUser);
 router
   .route("/:id")
   .delete(protect, admin, deleteUser)
-  .get(protect, admin, getUserById)
+  .get(protect, admin, getUser)
   .put(protect, admin, updateUser);
 
 module.exports = router;
