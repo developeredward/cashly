41179722172e7697073337c7f926bb2d950afc53
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-01-19 01:49:26 +0100

    Transactions Schema

---
+++ backend/models/TransactionModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const TransactionSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  accountId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Account",
+    required: true,
+  },
+
+  amount: {
+    type: Number,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Transaction", TransactionSchema);
