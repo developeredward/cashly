6e388bb390b26131b73461cd43596cbf71fd3760
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-01-19 01:55:39 +0100

    Category Schema

---
+++ backend/models/CategoryModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const CategorySchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+    },
+    type: {
+      type: String,
+      required: [true, "Type is required"],
+      enum: ["Income", "Expense"],
+    },
+    icon: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Category", CategorySchema);
