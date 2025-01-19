90b0a56c35cdd8fff1f4721bdf7e2bb7d0c5e11e
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-01-19 01:58:20 +0100

    Budget Schema

---
+++ backend/models/BudgetModel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const BudgetSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: [true, "Amount is required"],
+    },
+    period: {
+      type: String,
+      required: [true, "Period is required"],
+      enum: ["Daily", "Weekly", "Monthly", "Yearly"],
+    },
+    startDate: {
+      type: Date,
+      required: [true, "Start date is required"],
+    },
+    endDate: {
+      type: Date,
+      required: [true, "End date is required"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Budget", BudgetSchema);
