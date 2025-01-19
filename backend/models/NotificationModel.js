7a2fce53a5cece383bfb0ca406d6825f81f81e80
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-01-19 02:29:53 +0100

    Notifications Schema

---
+++ backend/models/NotificationModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const NotificationSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      required: [true, "Type is required"],
+      enum: ["info", "important", "warning"],
+    },
+    message: {
+      type: String,
+      required: [true, "Message is required"],
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Notification", NotificationSchema);
