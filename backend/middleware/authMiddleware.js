d8d3a0cafda21f4ab61e031246057e4f7b6e4815
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-01-19 16:10:28 +0100

    Authentication Route and Validation

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,46 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/UserModel");
+
+// Middleware to protect routes
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      // Verify the token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Get the user from the token, excluding the password
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+// Middleware to check for admin privileges
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(403);
+    throw new Error("Not authorized as an admin");
+  }
+};
+
+module.exports = { protect, admin };
