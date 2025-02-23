bd7c854d8ff9f2341c7c67b4669a64a8b0a52380
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-02-23 23:02:22 +0000

    Tab screens

---
+++ cashly/app/(tabs)/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+import { useTheme } from "@react-navigation/native";
+import { useAuth } from "../../context/AppContext";
+import PrimaryBtn from "../../components/Buttons/PrimaryBtn";
+import Loading from "../../components/Loading";
+const Home = () => {
+  const { logout } = useAuth();
+  const { colors } = useTheme();
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={{ color: colors.text, fontSize: 60 }}>Home</Text>
+      {/* <Loading /> */}
+      <PrimaryBtn title="Logout" onPress={logout || (() => {})} />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
