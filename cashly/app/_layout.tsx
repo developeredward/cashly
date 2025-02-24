70eac347bd77e5816a86d40c07d712881ac5ed2e
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-02-24 01:08:33 +0000

    fonts inc

--- cashly/app/_layout.tsx
+++ cashly/app/_layout.tsx
@@ -4,6 +4,7 @@ import { Stack } from "expo-router";
 import { Appearance } from "react-native";
 import { useEffect, useState } from "react";
 import { ThemeProvider } from "@react-navigation/native";
+import * as Font from "expo-font";
 import Welcome from "./Welcome";
 import DarkTheme from "../theme/DarkTheme";
 import LightTheme from "../theme/LightTheme";
@@ -11,15 +12,33 @@ import { AuthProvider, useAuth } from "../context/AppContext";
 
 export default function RootLayoutNav() {
   const [isDarkTheme, setIsDarkTheme] = useState<boolean>(false);
+  const [fontsLoaded, setFontsLoaded] = useState<boolean>(false);
 
   useEffect(() => {
-    // Detect system theme
     const currentTheme = Appearance.getColorScheme() === "dark";
     setIsDarkTheme(currentTheme);
   }, []);
+  useEffect(() => {
+    const loadFonts = async () => {
+      await Font.loadAsync({
+        "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+        "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+        "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+        "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+        "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+        "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+      });
+      setFontsLoaded(true);
+    };
+
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
-    // Wrap the entire app in the AuthProvider
     <AuthProvider>
       <AuthContent isDarkTheme={isDarkTheme} />
     </AuthProvider>
