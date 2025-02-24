4c72bf2c2610846f6629f476ae849895071a7efc
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-02-24 06:54:24 +0000

    Record Transaction screen

---
+++ cashly/app/RecordTransaction.tsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import React from "react";
+
+const RecordTransactionScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text style={{ color: "white" }}>Transactions</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default RecordTransactionScreen;
