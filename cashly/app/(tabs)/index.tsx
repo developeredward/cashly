0671353c82ef0d75c448050dfa40550e307a226e
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-02-24 05:04:52 +0000

    home page ui complete

--- cashly/app/(tabs)/index.tsx
+++ cashly/app/(tabs)/index.tsx
@@ -1,25 +1,83 @@
-import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, Text, View, StyleSheet } from "react-native";
 import { useTheme } from "@react-navigation/native";
 import { useAuth } from "../../context/AppContext";
+import { Ionicons, SimpleLineIcons } from "@expo/vector-icons";
 import PrimaryBtn from "../../components/Buttons/PrimaryBtn";
 import Loading from "../../components/Loading";
+import NotificationHandler from "../../components/Buttons/NotificationHandler";
+import Balance from "../../components/Balance/Balance";
+import Cards from "../../components/Cards/Cards";
+import TransactionsHandler from "../../components/Buttons/TransactionsHandler";
+import TransactionsSheet from "../../components/Sheets/TransactionsSheet";
 const Home = () => {
   const { logout } = useAuth();
+  const { authState } = useAuth();
   const { colors } = useTheme();
+  const [profile, setProfile] = useState<any>(null);
+
+  useEffect(() => {
+    if (authState?.user) {
+      setProfile(authState.user);
+    }
+  }, [authState?.authenticated]);
+
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ color: colors.text, fontSize: 60 }}>Home</Text>
-      {/* <Loading /> */}
-      <PrimaryBtn title="Logout" onPress={logout || (() => {})} />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.wrappedContainer}>
+        <View style={styles.headContainer}>
+          <View>
+            <Text style={[styles.heading, { color: colors.text }]}>
+              Hi, {profile?.name.split(" ")[0]}
+            </Text>
+            <Text style={[styles.subHeading, { color: colors.text + "60" }]}>
+              Welcome Back!
+            </Text>
+          </View>
+          <NotificationHandler />
+        </View>
+        <View>
+          <Balance color={colors.text} />
+          <Cards color={colors.text} background={colors.primary} />
+          <TransactionsHandler
+            color={colors.text}
+            background={colors.primary}
+          />
+        </View>
+      </View>
+      <View>
+        <TransactionsSheet
+          color={colors.text}
+          primary={colors.primary}
+          background={colors.background}
+        />
+      </View>
     </SafeAreaView>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  heading: {
+    fontSize: 30,
+    fontFamily: "Poppins-Bold",
+  },
+  subHeading: {
+    fontSize: 16,
+    fontFamily: "Poppins-Regular",
+  },
+  wrappedContainer: {
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  headContainer: {
+    marginTop: 20,
+    marginBottom: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
 
 export default Home;
