f62d2c3bb169e095da70ee194671a250cbe0586c
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-02-24 05:03:57 +0000

    logos

---
+++ cashly/constants/Logos.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  MaterialCommunityIcons,
+  AntDesign,
+  Entypo,
+  FontAwesome,
+} from "@expo/vector-icons";
+
+export interface LogosProps {
+  netflix: JSX.Element;
+  amazon: JSX.Element;
+  apple: JSX.Element;
+  google: JSX.Element;
+  spotify: JSX.Element;
+  wifi: JSX.Element;
+  bus: JSX.Element;
+  salary: JSX.Element;
+}
+
+export const logos: LogosProps = {
+  netflix: <MaterialCommunityIcons name="netflix" size={24} color="red" />,
+  amazon: <AntDesign name="amazon" size={24} color="black" />,
+  apple: <AntDesign name="apple1" size={24} color="black" />,
+  google: <AntDesign name="google" size={24} color="#4185F2" />,
+  spotify: <Entypo name="spotify" size={24} color="#31D165" />,
+  wifi: <AntDesign name="wifi" size={24} color="black" />,
+  bus: <FontAwesome name="bus" size={24} color="black" />,
+  salary: <MaterialCommunityIcons name="bank" size={24} color="#F4CB52" />,
+};
