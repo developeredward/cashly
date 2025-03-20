db92535c820f2f48f7a11293625046bb5ef7183a
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-03-20 20:25:50 +0000

    change from put to pathc

--- backend/routes/accountRoutes.js
+++ backend/routes/accountRoutes.js
@@ -14,6 +14,6 @@ router
   .route("/:id")
   .get(protect, getAccount)
   .delete(protect, deleteAccount)
-  .put(protect, updateAccount);
+  .patch(protect, updateAccount);
 
 module.exports = router;
