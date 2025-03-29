e6eac4f80269b44e9fec24137f9ad0e04dfda11b
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-03-29 02:35:59 +0000

    added loading state to true on intial mount

--- cashly/context/AppContext.tsx
+++ cashly/context/AppContext.tsx
@@ -36,7 +36,7 @@ export const AuthProvider = ({ children }: any) => {
     token: null,
     user: null,
     authenticated: false,
-    loading: false,
+    loading: true,
   });
 
   useEffect(() => {
