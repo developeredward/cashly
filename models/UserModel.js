faae22d8c0cd034c2fc7470569c98dda14e5e6f7
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-04-02 20:34:27 +0000

    Update UserModel.js

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const Transaction = require("./TransactionModel");
+const Account = require("./AccountModel");
+const Budget = require("./BudgetModel");
+const Goal = require("./GoalModel");
+const Settings = require("./SettingModel");
+const UserSummary = require("./UserSummaryModel");
+const RecurringTransaction = require("./RecurringTransactionModel");
+const Notification = require("./NotificationModel");
 
 const UserSchema = new mongoose.Schema(
   {
@@ -42,5 +50,30 @@ UserSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+UserSchema.pre(
+  "deleteOne",
+  { document: true, query: false },
+  async function (next) {
+    const userId = this._id;
+    console.log(
+      `Cascade deleting transactions and accounts for user: ${userId}`
+    );
+
+    await Transaction.deleteMany({ userId });
+    await Account.deleteMany({ userId });
+    await Budget.deleteMany({ userId });
+    await Goal.deleteMany({ userId });
+    await Settings.deleteMany({ userId });
+    await UserSummary.deleteMany({ userId });
+    await RecurringTransaction.deleteMany({ userId });
+    await Notification.deleteMany({ userId });
+    console.log("Cascade delete completed.");
+    
+
+    console.log("Related transactions and accounts deleted.");
+    next();
+  }
+);
 
 module.exports = mongoose.model("User", UserSchema);
