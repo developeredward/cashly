e95276045b4e1a9cdd1f7f6fd347a734673847f4
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-04-02 15:58:37 +0000

    Card screen

--- cashly/app/(tabs)/Card.tsx
+++ cashly/app/(tabs)/Card.tsx
@@ -1,11 +1,44 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
+import { useTheme } from "@react-navigation/native";
 import React from "react";
 
 const Card = () => {
+  const { colors } = useTheme();
   return (
-    <View>
-      <Text>Card</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: colors.background }}>
+      <View
+        style={{
+          padding: 20,
+          borderRadius: 10,
+          margin: 10,
+        }}
+      >
+        <Text
+          style={{
+            color: colors.text,
+            fontSize: 18,
+            fontWeight: "bold",
+            textAlign: "center",
+          }}
+        >
+          Cards are not supported yet!
+        </Text>
+        <View
+          style={{
+            marginTop: 20,
+            padding: 10,
+            borderWidth: 1,
+            borderColor: colors.border,
+            borderRadius: 5,
+            backgroundColor: colors.background,
+          }}
+        >
+          <Text style={{ color: colors.text, textAlign: "center" }}>
+            Please check back later for updates.
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
