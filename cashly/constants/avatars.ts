c35fc8ac0e8eb5a294450fccbb59c81b99158f12
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-04-02 17:05:50 +0000

    Avatrs mapping

---
+++ cashly/constants/avatars.ts
@@ -0,0 +1,29 @@
+export const Avatars = [
+  {
+    id: 1,
+    name: "a",
+    image: require("../assets/avatars/a.png"),
+  },
+  {
+    id: 2,
+    name: "b",
+    image: require("../assets/avatars/b.png"),
+  },
+  {
+    id: 3,
+    name: "c",
+    image: require("../assets/avatars/c.png"),
+  },
+  {
+    id: 4,
+    name: "d",
+    image: require("../assets/avatars/d.png"),
+  },
+];
+
+export const AvatarImageMapping = {
+  a: require("../assets/avatars/a.png"),
+  b: require("../assets/avatars/b.png"),
+  c: require("../assets/avatars/c.png"),
+  d: require("../assets/avatars/d.png"),
+};
