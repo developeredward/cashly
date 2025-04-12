544baf500e795a7f1f53ca7ec38a5786a780808a
Berthjone Redely Edward <edwardberth1@gmail.com>
2025-04-12 08:36:39 +0100

    Update UserModel.js

--- models/UserModel.js
+++ models/UserModel.js
@@ -28,7 +28,7 @@ const UserSchema = new mongoose.Schema(
     },
     currency: {
       type: String,
-      default: "USD",
+      default: "MAD",
       enum: ["USD", "EUR", "GBP", "NGN", "MAD"],
     },
     createdAt: {
